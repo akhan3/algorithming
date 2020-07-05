13a24b8f757af4e94bf5e971542448f41937392b
Aamir Khan <akhan@paradromics.com>
2020-07-05 03:35:47 -0500

    final solution

--- codingbat/recursion1.java
+++ codingbat/recursion1.java
@@ -471,4 +471,19 @@ public class recursion1 {
   }
 
 
+  // Given a string and a non-empty substring sub, compute recursively the largest substring which starts and ends with sub and return its length.
+  // strDist("catcowcat", "cat") → 9
+  // strDist("catcowcat", "cow") → 3
+  // strDist("cccatcowcatxx", "cat") → 9
+  public int strDist(String str, String sub) {
+    if (str.isEmpty())
+      return 0;
+    else if (str.indexOf(sub) != 0)
+      return strDist(str.substring(1), sub);
+    else if (str.lastIndexOf(sub) != str.length() - sub.length())
+      return strDist(str.substring(0, str.length() - 1), sub);
+    else
+      return str.length();
+  }
+
 }

ea1f31c450d3a4a047a67ac068e01ecd5aaa4b8a
Aamir Khan <akhan@paradromics.com>
2020-07-05 03:25:41 -0500

    Clean test case and instructions to compile

--- codingbat/testbenches/Main.java
+++ codingbat/testbenches/Main.java
@@ -1,46 +1,35 @@
+// How to compile and run
+// javac Main.java && java -ea Main
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This will be printed");
-        // strCopies("catcowcat", "cat", 2);
-        // strCopies("catcowcat", "cow", 2);
-        // strCopies("catcowcat", "cow", 1);
-        // strCopies("iiijjj", "i", 3);
-        // strCopies("iiijjj", "i", 4);
-        // strCopies("iiijjj", "ii", 2);
-        // strCopies("iiijjj", "ii", 3);
-        // strCopies("iiijjj", "x", 3);
-        strCopies("iiijjj", "x", 0);
-        // strCopies("iiiiij", "iii", 3);
-        // strCopies("iiiiij", "iii", 4);
-        // strCopies("ijiiiiij", "iiii", 2);
-        // strCopies("ijiiiiij", "iiii", 3);
-        // strCopies("dogcatdogcat", "dog", 2);
 
-
+        assert strDist("catcowcat", "cat") == 9;
+        assert strDist("catcowcat", "cow") == 3;
+        assert strDist("cccatcowcatxx", "cat") == 9;
+        assert strDist("abccatcowcatcatxyz", "cat") == 12;
+        assert strDist("xyx", "x") == 3;
+        assert strDist("xyx", "y") == 1;
+        assert strDist("xyx", "z") == 0;
+        assert strDist("z", "z") == 1;
+        assert strDist("x", "z") == 0;
+        assert strDist("", "z") == 0;
+        assert strDist("hiHellohihihi", "hi") == 13;
+        assert strDist("hiHellohihihi", "hih") == 5;
+        assert strDist("hiHellohihihi", "o") == 1;
+        assert strDist("hiHellohihihi", "ll") == 2;
     }
 
-
-
-
-    public static boolean strCopies(String str, String sub, int n) {
-        int count = 0;
-        boolean a = strCopies(str, sub, n, count);
-        System.out.println("a = " + a);
-        return a;
-    }
-
-    public static boolean strCopies(String str, String sub, int n, int count) {
-        if (n == 0)
-            return true;
-        else if (str.isEmpty())
-            return false;
-        else if (str.indexOf(sub) == 0)
-            if (++count >= n)
-                return true;
-            else
-                return strCopies(str.substring(1), sub, n, count);
-        else
-            return strCopies(str.substring(1), sub, n, count);
+    // Given a string and a non-empty substring sub, compute recursively the largest substring which starts and ends with sub and return its length.
+    public static int strDist(String str, String sub) {
+        if (str.isEmpty())
+          return 0;
+        else if (str.indexOf(sub) != 0)
+            return strDist(str.substring(1), sub);
+        else if (str.lastIndexOf(sub) != str.length() - sub.length())
+            return strDist(str.substring(0, str.length() - 1), sub);
+        return str.length();
     }
 
 }

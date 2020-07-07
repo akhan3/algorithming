01e2cd95b5cf372e82d746bcd6693d700c905d37
Aamir Khan <akhan@paradromics.com>
2020-07-07 13:04:23 -0500

    finish optimized implementation of two_sum_unsorted

--- homeworks/two_sum.py
+++ homeworks/two_sum.py
@@ -14,6 +14,22 @@ def two_sum_unsorted_brute_force(nums, target):
     return False
 
 
+def two_sum_unsorted_optimized(nums, target):
+    # if the array doesn't have two elements to begin with, bail out
+    if len(nums) < 2:
+        return False
+    cache = set()
+    # iterate over elements: O(n)
+    for N in nums:
+        # this check implements (n1+n2 == k) and is specific to two-sum problem
+        if N in cache:  # look-up in O(1)
+            return True
+        # save this element's complement for later look-up to avoid a nested loop
+        cache.add(target - N)  # insertion in O(1)
+    # if we reach here, it is because no match was found
+    return False
+
+
 def main():
     # just run test cases and return the exit code
     return pytest.main(["-v", "--capture=no"])

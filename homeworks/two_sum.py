5b54b3ef63294268c8324ed73ab8c4b9778ddf1e
Aamir Khan <akhan@paradromics.com>
2020-07-07 12:18:37 -0500

    finish brute force iplementation of two_sum_unsorted

--- homeworks/two_sum.py
+++ homeworks/two_sum.py
@@ -1,38 +1,22 @@
 import pytest
 
 
-def two_sum_sorted(nums, target):
-    print()
-    # zero sum can always be produced with an empty subset
-    # TODO: This is only true for group-sum but nor for two-sum problem
-    if target == 0:
-        return True
-
-    # if len(nums) == 0:
-    #     if target == 0:
-    #         return True
-    #     else:
-    #         return False
-    # if len(nums) == 1:
-    #     if target == nums[0]:
-    #         return True
-    #     else:
-    #         return False
-
-    print("starting for loops...")
+def two_sum_unsorted_brute_force(nums, target):
+    # if the array doesn't have two elements to begin with, bail out
+    if len(nums) < 2:
+        return False
+    # following are the brute-force loops: O(n^2)
     for k1, N1 in enumerate(nums):
-        if N1 == target:  # this check is not correct for two-sum problem
-            return True
-        nums2 = nums[k1 + 1 :]
-        print(k1, nums, nums2)
-        for k2, N2 in enumerate(nums):
+        for N2 in nums[k1 + 1 :]:
             if N1 + N2 == target:  # this check is specific to two-sum problem
                 return True
+    # if we reach here, it is because no match was found
     return False
 
 
 def main():
-    return pytest.main(["-vxl", "--capture=no"])
+    # just run test cases and return the exit code
+    return pytest.main(["-v", "--capture=no"])
 
 
 if __name__ == "__main__":

5cd22fa92b16afc452e84ea0001f1e692c362b53
Aamir Khan <akhan@paradromics.com>
2020-07-07 13:18:56 -0500

    improve inline comments

--- homeworks/two_sum.py
+++ homeworks/two_sum.py
@@ -2,32 +2,26 @@ import pytest
 
 
 def two_sum_unsorted_brute_force(nums, target):
-    # if the array doesn't have two elements to begin with, bail out
-    if len(nums) < 2:
+    if len(nums) < 2:  # if the array doesn't have two elements to begin with, bail out
         return False
-    # following are the brute-force loops: O(n^2)
-    for k1, N1 in enumerate(nums):
+    for k1, N1 in enumerate(nums):  # brute-force loops: O(n^2)
         for N2 in nums[k1 + 1 :]:
             if N1 + N2 == target:  # this check is specific to two-sum problem
-                return True
-    # if we reach here, it is because no match was found
-    return False
+                return True  # TADA!
+    return False  # if we reach here, it is because no match was found
 
 
 def two_sum_unsorted_optimized(nums, target):
-    # if the array doesn't have two elements to begin with, bail out
-    if len(nums) < 2:
+    if len(nums) < 2:  # if the array doesn't have two elements to begin with, bail out
         return False
-    cache = set()
-    # iterate over elements: O(n)
-    for N in nums:
-        # this check implements (n1+n2 == k) and is specific to two-sum problem
+    cache = set()  # initialize a cache container
+    for N in nums:  # iterate over elements: O(n)
         if N in cache:  # look-up in O(1)
-            return True
-        # save this element's complement for later look-up to avoid a nested loop
+            # it implements the check (n1+n2 == target) and is specific to two-sum problem
+            return True  # TADA!
         cache.add(target - N)  # insertion in O(1)
-    # if we reach here, it is because no match was found
-    return False
+        # save this element's complement for later look-up to avoid a nested loop
+    return False  # if we reach here, it is because no match was found
 
 
 def main():

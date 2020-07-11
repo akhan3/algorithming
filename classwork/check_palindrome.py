e5260cc25684ba4793c70bdf0207441f1b7e8fa8
Aamir Khan <akhan@paradromics.com>
2020-07-11 01:20:31 -0500

    add test infrastructure

--- classwork/check_palindrome.py
+++ classwork/check_palindrome.py
@@ -1,4 +1,8 @@
-def isPalindrome(s):
+import pytest
+import json
+
+
+def check_palindrome(s):
     # print(s)
     if len(s) <= 1:
         return True
@@ -6,12 +10,39 @@ def isPalindrome(s):
         return False
     # if s[0] == s[-1] and len(s) == 2:
     #     return True
-    return isPalindrome(s[1:-1])  # trim 1st and last character and recurse
+    return check_palindrome(s[1:-1])  # trim 1st and last character and recurse
+
+
+def load_test_cases(filename):
+    with open(filename, "r") as fh:
+        txt = json.load(fh)
+    txt = txt["check_palindrome_test_vectors_001"]
+    tc = list()
+    for t in txt:
+        tc.append((t["in"], t["out"]))
+    return tc
+
+
+loaded_test_cases = pytest.mark.parametrize(
+    "input, expected", load_test_cases("test_vectors.json")
+)
+
+
+@loaded_test_cases
+def test_check_palindrome(input, expected):
+    assert check_palindrome(input) == expected
+
+
+def main():
+    # just run test cases and return the exit code
+    # __file__ is necessary otherwise pytest cannot find any tests
+    return pytest.main(["-l", "--capture=no", __file__])
 
 
 if __name__ == "__main__":
-    assert isPalindrome("racecar") == True
-    assert isPalindrome("abba") == True
-    assert isPalindrome("a") == True
-    assert isPalindrome("hello") == False
-    assert isPalindrome("ax") == False
+    main()
+    # assert check_palindrome("")
+    # assert check_palindrome("")
+    # assert check_palindrome("a")
+    # assert not check_palindrome("hello")
+    # assert not check_palindrome("ax")
--- classwork/robo_sum_max.py
+++ classwork/robo_sum_max.py
@@ -1,10 +1,12 @@
 import json
 import random
+import pytest
 
+# Seed the random generator for repeatability
 random.seed(0)
 
 
-def roboSum(M, row=0, col=0):
+def robo_sum_max(M, row=0, col=0):
     nR = len(M)
     if nR == 0:
         return 0
@@ -19,16 +21,42 @@ def roboSum(M, row=0, col=0):
 
     # corner cases
     if col == nC - 1:  # if you hit the right wall
-        return M[row][col] + roboSum(M, row + 1, col)
+        return M[row][col] + robo_sum_max(M, row + 1, col)
     if row == nR - 1:  # if you hit the bottom wall
-        return M[row][col] + roboSum(M, row, col + 1)
+        return M[row][col] + robo_sum_max(M, row, col + 1)
 
-    rght = roboSum(M, row, col + 1)
-    down = roboSum(M, row + 1, col)
+    rght = robo_sum_max(M, row, col + 1)
+    down = robo_sum_max(M, row + 1, col)
     return M[row][col] + max(rght, down)
 
 
-if __name__ == "__main__":
+def load_test_cases(filename):
+    with open(filename, "r") as fh:
+        txt = json.load(fh)
+    txt = txt["robo_sum_max_test_vectors_001"]
+    tc = list()
+    for t in txt:
+        tc.append((t["in"], t["out"]))
+    return tc
+
+
+loaded_test_cases = pytest.mark.parametrize(
+    "input, expected", load_test_cases("test_vectors.json")
+)
+
+
+@loaded_test_cases
+def test_robo_sum_max(input, expected):
+    assert robo_sum_max(input) == expected
+
+
+def main():
+    # just run test cases and return the exit code
+    # __file__ is necessary otherwise pytest cannot find any tests
+    return pytest.main(["-l", "--capture=no", __file__])
+
+
+def produce_test_vectors():
     results = list()
     MAX = 10
     n = 5
@@ -38,7 +66,12 @@ if __name__ == "__main__":
         M = list(list())
         for k in range(n):
             M.append(random.sample(range(MAX), m))
-        ans = roboSum(M)
+        ans = robo_sum_max(M)
         results.append({"in": M, "out": ans})
     txt = json.dumps({"robo_sum_max_test_vectors_001": results})
     print(txt)
+
+
+if __name__ == "__main__":
+    main()
+    # produce_test_vectors()

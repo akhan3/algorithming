72b313465d6d20b0bf951c8bf2ff118caafc2ee5
Aamir Khan <akhan@paradromics.com>
2020-07-11 04:29:02 -0500

    wrap long comment lines
    comments

--- codingbat/recursion2.java
+++ codingbat/recursion2.java
@@ -61,7 +61,10 @@ public class recursion2 {
   }
 
 
-  // Given an array of ints, is it possible to choose a group of some of the ints, beginning at the start index, such that the group sums to the given target? However, with the additional constraint that all 6's must be chosen. (No loops needed.)
+  // Given an array of ints, is it possible to choose a group of some of the
+  // ints, beginning at the start index, such that the group sums to the given
+  // target? However, with the additional constraint that all 6's must be chosen.
+  // (No loops needed.)
   // groupSum6(0, [5, 6, 2], 8) → true
   // groupSum6(0, [5, 6, 2], 9) → false
   // groupSum6(0, [5, 6, 2], 7) → false
@@ -98,7 +101,10 @@ public class recursion2 {
   }
 
 
-  // Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given target with this additional constraint: If a value in the array is chosen to be in the group, the value immediately following it in the array must not be chosen. (No loops needed.)
+  // Given an array of ints, is it possible to choose a group of some of the
+  // ints, such that the group sums to the given target with this additional
+  // constraint: If a value in the array is chosen to be in the group, the value
+  // immediately following it in the array must not be chosen. (No loops needed.)
   // groupNoAdj(0, [2, 5, 10, 4], 12) → true
   // groupNoAdj(0, [2, 5, 10, 4], 14) → false
   // groupNoAdj(0, [2, 5, 10, 4], 7) → false
@@ -124,7 +130,11 @@ public class recursion2 {
 
 
 
-  // Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given target with these additional constraints: all multiples of 5 in the array must be included in the group. If the value immediately following a multiple of 5 is 1, it must not be chosen. (No loops needed.)
+  // Given an array of ints, is it possible to choose a group of some of the
+  // ints, such that the group sums to the given target with these additional
+  // constraints: all multiples of 5 in the array must be included in the group.
+  // If the value immediately following a multiple of 5 is 1, it must not be chosen.
+  // (No loops needed.)
   // groupSum5(0, [2, 5, 10, 4], 19) → true
   // groupSum5(0, [2, 5, 10, 4], 17) → true
   // groupSum5(0, [2, 5, 10, 4], 12) → false

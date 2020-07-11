af2adbf5589ab81d6ffac155ca520a079b1da864
Aamir Khan <akhan@paradromics.com>
2020-07-11 02:55:27 -0500

    groupNoAdj: only one test case is failing // groupNoAdj(0, [2, 5, 10, 4, 2], 7) → true // only this test is fasiling

--- codingbat/recursion2.java
+++ codingbat/recursion2.java
@@ -102,5 +102,33 @@ public class recursion2 {
   }
 
 
+  // Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given target with this additional constraint: If a value in the array is chosen to be in the group, the value immediately following it in the array must not be chosen. (No loops needed.)
+  // groupNoAdj(0, [2, 5, 10, 4], 12) → true
+  // groupNoAdj(0, [2, 5, 10, 4], 14) → false
+  // groupNoAdj(0, [2, 5, 10, 4], 7) → false
+  // groupNoAdj(0, [2, 5, 10, 4, 2], 7) → true // only this test is fasiling
+  public boolean groupNoAdj(int start, int[] nums, int target) {
+      int accum;
+      accum = 0;  // accumulator to hold running sum
+      boolean sum0 = groupNoAdjAux(start, nums, target, accum);
+      accum = 0;  // accumulator to hold running sum
+      boolean sum1 = groupNoAdjAux(start+1, nums, target, accum);
+      return sum0 || sum1;
+  }
+  public boolean groupNoAdjAux(int start, int[] nums, int target, int accum) {
+    if (start >= nums.length)
+      return accum == target;
+    accum += nums[start];
+    if (groupNoAdjAux(start + 2, nums, target, accum))
+      return true;
+    accum -= nums[start];
+    if (groupNoAdjAux(start + 2, nums, target, accum))
+      return true;
+    return false;
+  }
+
+
+
+
 }
 

9ef85f4d59aaa7da268adc6b40b1d6b7268a6edb
Aamir Khan <akhan@paradromics.com>
2020-07-12 12:20:19 -0500

    implement power set without Python stack

--- classwork/generate_power_set.py
+++ classwork/generate_power_set.py
@@ -13,9 +13,28 @@ def generate_power_set(S, head=0, container=list()):
     return
 
 
+def generate_power_set_no_stack(S, s=0, Q=[], q=0):
+    if len(Q) == 0:
+        Q = [None] * len(S)
+    if s == len(S):
+        print(Q[:q])
+        return
+    # select this element
+    Q[q] = S[s]
+    generate_power_set_no_stack(S, s + 1, Q, q + 1)
+    # do NOT select this element
+    generate_power_set_no_stack(S, s + 1, Q, q)
+    return
+
+
 if __name__ == "__main__":
-    generate_power_set([1, 2, 3])
+    myset = [1, 2, 3, 4]
+    generate_power_set(myset)
     print("----------------------------------------------------")
-    generate_power_set("abc")
-    print("----------------------------------------------------")
-    generate_power_set([10, 20, 30, 40, 50])
+    generate_power_set_no_stack(myset)
+
+    # generate_power_set([1, 2, 3])
+    # print("----------------------------------------------------")
+    # generate_power_set("abc")
+    # print("----------------------------------------------------")
+    # generate_power_set([10, 20, 30, 40, 50])

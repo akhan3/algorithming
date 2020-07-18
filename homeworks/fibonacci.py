6385272fe3a066e01e012450787e44b99674b492
Aamir Khan <akhan@paradromics.com>
2020-07-18 01:55:23 -0500

    fibonacci done

--- homeworks/fibonacci.py
+++ homeworks/fibonacci.py
@@ -1,17 +1,23 @@
-import pytest
+#!/usr/bin/env python3
 
 
-def fibonacci(n, stack=[]):
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def fibonacci_call_stack(n, stack=list()):
     if n <= 1:
         return n
 
     stack.append(n - 1)
-    a1 = fibonacci(n - 1, stack)
+    a1 = fibonacci_call_stack(n - 1, stack)
     stack[-1] = {n - 1: a1}
     print(stack)
 
     stack.append(n - 2)
-    a2 = fibonacci(n - 2, stack)
+    a2 = fibonacci_call_stack(n - 2, stack)
     stack[-1] = {n - 2: a2}
     print(stack)
     stack.pop()
@@ -21,4 +27,6 @@ def fibonacci(n, stack=[]):
 
 
 if __name__ == "__main__":
-    fibonacci(9)
+    x = 7
+    print(fibonacci(x))
+    fibonacci_call_stack(x)

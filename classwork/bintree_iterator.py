4d690f393d2d65e5b131b3bd974cabf64a21618e
Aamir Khan <akhan@paradromics.com>
2020-07-19 13:05:23 -0500

    binary tree iterator: working with exception handling

---
+++ classwork/bintree_iterator.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.stack = []
+
+    def __repr__(self):
+        left_val = None if self.left is None else self.left.value
+        right_val = None if self.right is None else self.right.value
+        return "({}, {}, {})".format(self.value, left_val, right_val)
+
+
+class BST_Iterator:
+    def __init__(self, node):
+        self.stack = []  # initialize empty stack
+        self._populate_stack(node)
+
+    def __repr__(self):
+        return str(self.stack)
+
+    def _populate_stack(self, node):
+        if node is None:
+            return
+        else:
+            self.stack.append(node)  # C (in-order traversal)
+            self._populate_stack(node.left)  # L (in-order traversal)
+
+    def next(self):
+        if self.has_next():
+            node = self.stack.pop()
+            self._populate_stack(node.right)  # R (in-order traversal)
+            return node
+        else:
+            raise IndexError("No more nodes!")
+
+    def has_next(self):
+        return len(self.stack) != 0
+
+
+def main():
+    #     4
+    #    / \
+    #   3   8
+    #  / \   \
+    # 2   5   1
+    #    /
+    #   6
+
+    n4 = Node(4)
+    n3 = Node(3)
+    n8 = Node(8)
+    n2 = Node(2)
+    n5 = Node(5)
+    n1 = Node(1)
+    n6 = Node(6)
+    root = n4
+    n4.left = n3
+    n4.right = n8
+    n3.left = n2
+    n3.right = n5
+    n5.left = n6
+    n8.right = n1
+
+    iterator = BST_Iterator(root)
+    for k in range(10):
+        try:
+            node = iterator.next()
+            print(iterator.has_next(), node)
+        except IndexError as exception:
+            print(exception)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Output should be in the following order
+# from tree_order_traversal.py
+#   print_in_order(root)  # 2, 3, 6, 5, 4, 8, 1,
+
+# $ python3 bst_iterator.py
+# True (2, None, None)
+# True (3, 2, 5)
+# True (6, None, None)
+# True (5, 6, None)
+# True (4, 3, 8)
+# True (8, None, 1)
+# False (1, None, None)
+# No more nodes!
+# No more nodes!
+# No more nodes!

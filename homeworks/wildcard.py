bb75e9f44e1cd90361462441b2268af8cd3c9874
Aamir Khan <akhan@paradromics.com>
2020-07-19 01:48:40 -0500

    wildcard: space efficient solution

--- homeworks/wildcard.py
+++ homeworks/wildcard.py
@@ -33,6 +33,21 @@ def wildcard_str_passing(input: str, output: str = "", i=0):
         wildcard_str_passing(input, output + input[i], i + 1)
 
 
+# this solution is space efficient since it manipulates a single list reference
+def wildcard_list_reference(input: str, output: list, i=0):
+    if i == len(input):
+        print("".join(output))
+        return
+    if input[i] == "?":
+        output[i] = "0"
+        wildcard_list_reference(input, output, i + 1)
+        output[i] = "1"
+        wildcard_list_reference(input, output, i + 1)
+    else:
+        output[i] = input[i]
+        wildcard_list_reference(input, output, i + 1)
+
+
 wildcard_test_cases = pytest.mark.parametrize(
     "input, output",
     [
@@ -47,6 +62,7 @@ wildcard_test_cases = pytest.mark.parametrize(
 def test_wildcard(input, output):
     assert wildcard(input, set(), list()) == output
     wildcard_str_passing(input)
+    wildcard_list_reference(input, [None] * len(input))
 
 
 def main():

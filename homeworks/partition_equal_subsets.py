1760f0ad5770485878af324ded9cb08d5191c485
Aamir Khan <akhan@paradromics.com>
2020-07-26 13:43:34 -0500

    Exam 1: embed problem statements as comments

--- homeworks/partition_equal_subsets.py
+++ homeworks/partition_equal_subsets.py
@@ -3,7 +3,31 @@
 import pytest
 
 
-# Partition Equal Subset Sum
+"""
+Recursion Test: Partition Equal Subset Sum
+==========================================
+
+Given a non-empty array containing only positive integers, find if the
+array can be partitioned into two subsets such that the sum of elements in
+both subsets is equal.
+
+Example 1:
+
+Input: [1, 5, 11, 5]
+
+Output: true
+
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+
+Example 2:
+
+Input: [1, 2, 3, 5]
+
+Output: false
+
+Explanation: The array cannot be partitioned into equal sum subsets.
+"""
 
 
 def partition_equal_subsets(
@@ -17,10 +41,7 @@ def partition_equal_subsets(
         return True
     # Build accumulators and recurse
     return partition_equal_subsets(
-        numbers,
-        pointer + 1,
-        accum1 + numbers[pointer],
-        accum2 - numbers[pointer],
+        numbers, pointer + 1, accum1 + numbers[pointer], accum2 - numbers[pointer]
     ) or partition_equal_subsets(numbers, pointer + 1, accum1, accum2)
 
 
